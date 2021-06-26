c6855b2be263f027d8ba20744f779ce93919b340
cairumeng <cairumeng@gmail.com>
2021-06-26 22:22:25 +0200

    add index page

--- frontend_client/src/pages/Index.js
+++ frontend_client/src/pages/Index.js
@@ -1,4 +1,25 @@
+import React, { useState } from 'react'
+
+import GoogleAddressLookup from 'react-rainbow-components/components/GoogleAddressLookup'
+
 const Index = () => {
-  return <h1>Here is the home page!</h1>
+  const [address, setAddress] = useState(null)
+
+  return (
+    <div
+      className="flex justify-center items-center p-2 flex-col"
+      style={{ height: 'calc(100vh - 4rem)' }}
+    >
+      <h1 className="font-bold text-4xl mb-5">Livrensemble</h1>
+
+      <GoogleAddressLookup
+        onChange={(value) => setAddress(value)}
+        value={address}
+        placeholder="Enter your address to find a restaurant"
+        apiKey="AIzaSyAGfmv0CeO4LyBVAX6fN-gdHmyNFLRfm9M"
+        className="w-full sm:w-1/2 lg:w-1/3"
+      />
+    </div>
+  )
 }
 export default Index
